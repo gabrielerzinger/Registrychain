c7d3fa3c8fa7ea46b77b9a15d0373aba3621885b
Gabriel Taets <gabrieltaets@gmail.com>
2018-06-02 19:57:30 -0300

    Repaginated Login Screen

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToasterService, Toaster } from 'angular2-toaster';
 
 import { UserService } from '../services/user.service';
 
@@ -12,16 +14,36 @@ export class LoginComponent implements OnInit {
 
     public username: string;
     public password: string;
+    @ViewChild('loginForm') loginForm: NgForm;
 
-    constructor(public userService: UserService, public router: Router) { }
+    constructor(public userService: UserService, public router: Router, public toasterService: ToasterService) { }
 
     ngOnInit() {
     }
 
     onSubmit(){
-        this.userService.login(this.username, this.password).subscribe(undefined, err => console.log(err), () => {
-            this.router.navigate(['/']);
-        });
+        // check validity
+        if(this.loginForm.valid) {
+            this.userService.login(this.username, this.password).subscribe((user) => {
+                this.router.navigate(['/']);
+            }, () => {
+                let toast: Toast = {
+                    type: 'error',
+                    title: 'Erro!',
+                    body: 'Credenciais incorretas!'
+                }
+                this.toasterService.pop(toast);
+            });
+        }
+        else {
+            let toast: Toast = {
+                type: 'error',
+                title: 'Erro!',
+                body: 'Por favor, preencha todos os campos!'
+            };
+            this.toasterService.pop(toast);
+        }
+
     }
 
 }
